f1b947234542d2d0673c02377e0b4b86fe9cf4c6
bnanupam <bnanupam@gmail.com>
2024-03-31 19:28:29 +0530

    api

--- src/constants/appAttachments.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const MOEI_FILE_UPLOAD_SOURCE = "MOEI";
-export const MOEI_FILE_UPLOAD_TYPE = "FP";
-
-export const ATTACHMENT_CONFIG: any = {
-  FUEL_PRICE_APPROVAL_ATTACHMENT_TYPE_MOM: "ATTYP_110",
-  FUEL_PRICE_EXCEL_TEMPLETE_DOCUMENT_MASTER_ID: "100",
-};
